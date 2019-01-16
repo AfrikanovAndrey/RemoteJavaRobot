02c27b2b2ef6318398944d3e3a2a55c88c0cd0c8
Afrikanov Andrey <a.afrikanov@propellerads.com>
2019-01-16 17:47:29 +0300

    Travis script

--- ci/docker_images_deploy.sh
+++ ci/docker_images_deploy.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 
-echo Hello
-echo $DOCKER_HUB_PASSWORD
-cd server/target
-ls
+echo "# Build Selenoid browser images"
+
+echo "### Build 'vnc_chrome_70' Docker image..."
+docker build -f ./../docker-images/selenoid/chrome/Dockerfile:vnc_chome_70 -t aafrikanov/selenoid_images_wth_remote_java_robot:vnc_chrome_70 ./../
+
+echo "# Login to DockerHub"
+echo $DOCKER_HUB_PASSWORD | docker login -u $DOCKER_HUB_USER_NAME --password-stdin
+
+echo "# Push 'aafrikanov/selenoid_images_wth_remote_java_robot:vnc_chrome_70' image ..."
+docker push aafrikanov/selenoid_images_wth_remote_java_robot:vnc_chrome_70
